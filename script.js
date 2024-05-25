245b06a8c3d33dee9835a5db3a8c1a2c28faf795
Rselva1097 <99159175+Rselva1097@users.noreply.github.com>
2024-05-25 07:09:21 -0400

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // complete the given function
 
 function palindrome(str){
+        const st=0;
+     	const ed=str.length()-1;
 
+	while(st < ed){
+		if(str.toLowerCase().charAt(st) != str.toLowerCase().charAt(ed)){
+			return false;
+		}
+		st++;
+		ed--;
+	}
+
+	return true;
 }
 module.exports = palindrome
